42051d1ef9aca7abf434dc183d0815a2d537135f
nvienot <nvienot@student.42.fr>
2019-04-08 23:37:13 +0200

    ajout cube2

--- index.js
+++ index.js
@@ -1,43 +1,111 @@
 var width = 500;
 var height = 500;
-
 var scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xffffff );
 var camera = new THREE.PerspectiveCamera(25, width/height, 0.1, 1000);
-
 var renderer = new THREE.WebGLRenderer();
+scene.background = new THREE.Color( 0xffffff );
+
 renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshFaceMaterial([
+    new THREE.MeshBasicMaterial({
+        color: 0x00ff00
+    }),
+    new THREE.MeshBasicMaterial({
+        color: 0xff0000
+    }),
+    new THREE.MeshBasicMaterial({
+        color: 0x0000ff,
+    }),
+    new THREE.MeshBasicMaterial({
+        color: 0xffff00
+    }),
+    new THREE.MeshBasicMaterial({
+        color: 0x00ffff
+    }),
+    new THREE.MeshBasicMaterial({
+        color: 0xff00ff
+    })
+]);
 
-// create the cube
-var geometry = new THREE.BoxGeometry(1,1,1);
-var material = new THREE.MeshPhongMaterial({
-  // ambient: 0x55555,
-  color: 0x55555,
-  // specular: 0xffffff,
-  // shininess: 50,
-  shading: THREE.SmoothShading
-});
 var cube = new THREE.Mesh(geometry, material);
+cube.rotation.x = Math.PI/4;
+cube.rotation.y = Math.PI/4;
 scene.add(cube);
 
-// create lights
 scene.add( new THREE.AmbientLight(0xffffff) );
 var light = new THREE.PointLight(0xffffff, 6, 40);
 light.position.set(20, 20, 20);
 scene.add(light);
 
-// set the camera
 camera.position.z = 10;
 
-// define an animation loop
-var render = function () {
-  requestAnimationFrame(render);
-  // rotate the cube
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  // renderer.setClearColor( 0xffffff, 0);
-  renderer.render(scene, camera);
+var isDragging = false;
+var previousMousePosition = {
+    x: 0,
+    y: 0
 };
 
+$(renderer.domElement).on('mousedown', function(e) {
+    isDragging = true;
+})
+
+.on('mousemove', function(e) {
+    var deltaMove = {
+        x: e.offsetX-previousMousePosition.x,
+        y: e.offsetY-previousMousePosition.y
+    };
+    if(isDragging) {  
+        var deltaRotationQuaternion = new THREE.Quaternion()
+        .setFromEuler(new THREE.Euler(toRadians(deltaMove.y * 1), toRadians(deltaMove.x * 1), 0, 'XYZ'));
+        cube.quaternion.multiplyQuaternions(deltaRotationQuaternion, cube.quaternion);
+    }
+    previousMousePosition = {
+        x: e.offsetX,
+        y: e.offsetY
+    };
+});
+
+$(document).on('mouseup', function(e) {
+    isDragging = false;
+});
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function(callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+var clic = false;
+
+$("#stop").on('click', function(e) {
+    if (clic == false)
+        clic = true;
+    else
+        clic = false; 
+});
+
+function render() {
+    renderer.render(scene, camera); 
+    if (clic == false) {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    }
+    requestAnimFrame(render);
+}
+
 render();
+update(0, totalGameTime);
+
+function toRadians(angle) {
+	return angle * (Math.PI / 180);
+}
+
+function toDegrees(angle) {
+	return angle * (180 / Math.PI);
+}
+
